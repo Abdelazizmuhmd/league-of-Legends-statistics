2fab2e9d2de72617285d2092881ea8041dca779d
abdelaziz <abdelazizwmuhmd@gmail.com>
2019-08-24 05:04:51 +0200

    interface without constrain

--- league of Legends statistics/Controllers/ViewController.swift
+++ league of Legends statistics/Controllers/ViewController.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource {
+    let regions : [String] = ["1","2","3","4","5","6","7","8","9","10","11"]
+    
 
     @IBOutlet weak var cameraDetection: UIButton!
-  
+    @IBOutlet weak var picker: UIPickerView!
     @IBOutlet weak var SummonerNameButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+          picker.delegate = self
         summonerNameButtonStyle(buttonStyle: cameraDetection)
         summonerNameButtonStyle(buttonStyle: SummonerNameButton)
       
@@ -23,8 +26,16 @@ class ViewController: UIViewController {
     
     
     
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
     
-    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return regions.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return regions[row]
+    }
     
     
     
