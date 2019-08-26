037bd668da23071363e3b5f5b072ad5dec98d277
abdelaziz <abdelazizwmuhmd@gmail.com>
2019-08-26 23:18:00 +0200

    we will start segues now

--- league of Legends statistics/Controllers/searchPageViewController.swift
+++ league of Legends statistics/Controllers/searchPageViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource {
+class searchPageViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource {
     let regions : [String] = ["1","2","3","4","5","6","7","8","9","10","11"]
     
 
